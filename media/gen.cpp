1b69b142802c6d8644d678b90c5f2af22e231ec3
Gourav Sahu <gauravsahu338@gmail.com>
2021-12-30 23:23:41 +0530

    updation to server commit

--- media/gen.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    string s;
-    cout<<"my name is docker";
-	return 0;
-}
--- online_judge/views.py
+++ online_judge/views.py
@@ -11,9 +11,6 @@ from .models import Testcase
 import subprocess,os,time
 from subprocess import PIPE
 
-import filecmp
-
-
 def index(request):
     question_name=Problems.objects.all()
     template=loader.get_template('online_judge/index.html')
@@ -39,7 +36,66 @@ def finalpage(request,question_id,form_id):
     
     cmd="media/"+str(question_id)+"/"+ myfile.name
     
-    data=subprocess.Popen(["docker","start", "my-gcc"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+    subprocess.run("sudo docker start my-gcc",shell=True)
+
+    subprocess.run("sudo docker cp "+cmd+" my-gcc:/gourav.cpp",shell=True)
+
+    input="media/"+str(question_id)+"/tests.txt"
+    subprocess.run("sudo docker cp "+input+" my-gcc:/tests.txt",shell=True)
+
+    output="media/out.txt"
+    subprocess.run("sudo docker cp "+output+" my-gcc:/out.txt",shell=True)
+
+    subprocess.run("sudo docker exec my-gcc g++ gourav.cpp ./a.out",shell=True)
+
+    command= 'sudo docker exec -ti my-gcc sh -c "./a.out <tests.txt > out.txt"'
+
+    subprocess.run(command,shell=True)
+    
+    subprocess.run("sudo docker exec my-gcc rm -rf a.out",shell=True)
+
+    command2= "sudo docker cp my-gcc:out.txt " +"media/"+str(question_id)+"/out.txt"
+    subprocess.run(command2,shell=True)
+
+    f1="media/"+str(question_id)+"/my-out.txt"
+    f2="media/"+str(question_id)+"/out.txt"
+    
+    file1=open(f1,"r")
+    file2=open(f2,"r")
+
+    lines1=file1.readlines()
+    lines2=file2.readlines()
+
+
+    if(lines1==lines2):
+        verdict="All test cases Passed"
+    else:
+        verdict="Please try again"
+        
+    return render(request,'online_judge/finalpage.html',{'name':verdict,'question':question_id})
+    
+
+
+# Create your views here.
+'''
+ subprocess.run("sudo docker start my-gcc",shell=True)
+
+    subprocess.run("sudo docker cp "+cmd+" my-gcc:/Gourav-Sahu.cpp",shell=True)
+
+    subprocess.run("sudo docker cp media/tests.txt my-gcc:/tests.txt",shell=True)
+
+    subprocess.run("sudo docker exec my-gcc g++ Gourav-Sahu.cpp ./a.out",shell=True)
+
+    command= 'sudo docker exec -ti my-gcc sh -c "./a.out <tests.txt > out.txt"'
+
+    subprocess.run(command,shell=True)
+
+    command2= "sudo docker cp my-gcc:/out.txt media/out.txt"
+    subprocess.run(command2,shell=True)
+'''
+
+'''
+data=subprocess.Popen(["docker","start", "my-gcc"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
     data.communicate()
 
     data=subprocess.Popen(["docker","cp",cmd,"my-gcc:/gourav.cpp"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
@@ -69,23 +125,6 @@ def finalpage(request,question_id,form_id):
     data=subprocess.Popen(command2,stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True,shell=True)
     data.communicate()
 
-    f1="media/"+str(question_id)+"/my-out.txt"
-    f2="media/"+str(question_id)+"/out.txt"
-    
-    file1=open(f1,"r")
-    file2=open(f2,"r")
-
-    lines1=file1.readlines()
-    lines2=file2.readlines()
 
 
-    if(lines1==lines2):
-        verdict="All test cases Passed"
-    else:
-        verdict="Please try again"
-        
-    return render(request,'online_judge/finalpage.html',{'name':verdict,'question':question_id})
-    
-
-
-# Create your views here.
+'''
